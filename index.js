ba14cb04e73a1fa6aa23a595ec7beb15d0a0aff6
Raj Anand <ra9122325614@gmail.com>
2025-10-25 17:40:14 +0530

    github data added

--- index.js
+++ index.js
@@ -4,6 +4,43 @@ const express = require('express')
 const app = express()   // app is a very powerful variable which holds the different framework like express, koa etc.
 const port = 3000     // there are different ports like usb,type-c etc. here we are using virtual port for server and it has more than 65000 ports in it. so it can any number from 0 to 65535.
 
+
+const githubData = {
+  "login": "BeingRoaster-Raj",
+  "id": 180921397,
+  "node_id": "U_kgDOCsikNQ",
+  "avatar_url": "https://avatars.githubusercontent.com/u/180921397?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/BeingRoaster-Raj",
+  "html_url": "https://github.com/BeingRoaster-Raj",
+  "followers_url": "https://api.github.com/users/BeingRoaster-Raj/followers",
+  "following_url": "https://api.github.com/users/BeingRoaster-Raj/following{/other_user}",
+  "gists_url": "https://api.github.com/users/BeingRoaster-Raj/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/BeingRoaster-Raj/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/BeingRoaster-Raj/subscriptions",
+  "organizations_url": "https://api.github.com/users/BeingRoaster-Raj/orgs",
+  "repos_url": "https://api.github.com/users/BeingRoaster-Raj/repos",
+  "events_url": "https://api.github.com/users/BeingRoaster-Raj/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/BeingRoaster-Raj/received_events",
+  "type": "User",
+  "user_view_type": "public",
+  "site_admin": false,
+  "name": null,
+  "company": null,
+  "blog": "",
+  "location": null,
+  "email": null,
+  "hireable": null,
+  "bio": null,
+  "twitter_username": null,
+  "public_repos": 16,
+  "public_gists": 0,
+  "followers": 0,
+  "following": 0,
+  "created_at": "2024-09-09T06:43:45Z",
+  "updated_at": "2025-09-22T08:13:48Z"
+}
+
 app.get('/', (req, res) => {           // here we are using get method to get the data from server. '/' means home route. req means request and res means response.
   res.send('Hello World!')
 })
@@ -33,4 +70,10 @@ app.get('/instagram', (req, res) => {   // here we are using get method to get t
  
 app.listen(process.env.PORT, () => {                        // here we are using listen method to listen the port. port means 3000. () => {} this is a call back function.
   console.log(`Example app listening on port ${port}`)
+})
+
+
+app.get('/github', (req, res) => {   // here we are using get method to get the data from server. '/github' means github route. req means request and res means response. 
+  // when we type localhost:3000/github in browser it will show githubData in json format.       and        () => {} this is a call back function.
+    res.json(githubData)
 })
